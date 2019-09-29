e6275b87a86eaac5c952858e6ea95e108d73250b
kakao <kwt20018@gmail.com>
2019-09-29 10:07:39 +0900

    ✅Sponsor

--- src/components/Sponsor.js
+++ src/components/Sponsor.js
@@ -13,34 +13,97 @@ export default class Sponsor extends Component {
                         </thead>
                         <tbody>
                         <tr>
-                            <td>ほげ</td>
+                            <td> エヌ・テック・システムズ</td>
                         </tr>
                         <tr>
-                            <td>ほげ</td>
+                            <td> おきぎんエス・ピー・オー</td>
                         </tr>
                         <tr>
-                            <td>ほげ</td>
+                            <td> 沖食商事</td>
                         </tr>
                         <tr>
-                            <td>ほげ</td>
+                            <td> 沖縄伊藤園</td>
                         </tr>
                         <tr>
-                            <td>ほげ</td>
+                            <td>沖縄ソフトウェアセンター</td>
                         </tr>
                         <tr>
-                            <td>ほげ</td>
+                            <td>沖縄富士通システムエンジニアリング</td>
                         </tr>
                         <tr>
-                            <td>ほげ</td>
+                            <td>沖縄ホーメル</td>
                         </tr>
                         <tr>
-                            <td>ほげ</td>
+                            <td> 開邦工業</td>
                         </tr>
                         <tr>
-                            <td>ほげ</td>
+                            <td> 興洋電子</td>
                         </tr>
                         <tr>
-                            <td>ほげ</td>
+                            <td>ジミー</td>
+                        </tr>
+                        <tr>
+                            <td> ビーンズラボ</td>
+                        </tr>
+                        <tr>
+                            <td> 屋部土建</td>
+                        </tr>
+                        <tr>
+                            <td> 琉球銀行</td>
+                        </tr>
+                        <tr>
+                            <td>ロワジール・ホテルズ沖縄</td>
+                        </tr>
+                        <tr>
+                            <td>オキコ</td>
+                        </tr>
+                        <tr>
+                            <td>沖電グローバルシステムズ</td>
+                        </tr>
+                        <tr>
+                            <td>沖縄菱電ビルシステムズ</td>
+                        </tr>
+                        <tr>
+                            <td>沖縄ターミナル</td>
+                        </tr>
+                        <tr>
+                            <td>沖縄ツーリスト 名護支店</td>
+                        </tr>
+                        <tr>
+                            <td>沖縄明治乳業</td>
+                        </tr>
+                        <tr>
+                            <td>沖縄ユーシーシーコーヒー</td>
+                        </tr>
+                        <tr>
+                            <td>沖縄ロジテム</td>
+                        </tr>
+                        <tr>
+                            <td>オリオンビール　名護工場</td>
+                        </tr>
+                        <tr>
+                            <td>昭和製紙</td>
+                        </tr>
+                        <tr>
+                            <td>シーサー Net </td>
+                        </tr>
+                        <tr>
+                            <td>全日本空輸 沖縄支社</td>
+                        </tr>
+                        <tr>
+                            <td>拓南本社</td>
+                        </tr>
+                        <tr>
+                            <td>日本トランスオーシャン航空</td>
+                        </tr>
+                        <tr>
+                            <td>ユーレストジャパン 沖縄高専店</td>
+                        </tr>
+                        <tr>
+                            <td>MRO Japan </td>
+                        </tr>
+                        <tr>
+                            <td>SCSKニアショアシステムズ</td>
                         </tr>
                         </tbody>
                     </table>
