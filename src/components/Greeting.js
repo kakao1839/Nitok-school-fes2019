f07c60246dcee6a5c39abb0f1632d8a7911637d6
KAKAO <38216541+kakao1839@users.noreply.github.com>
2019-09-29 09:25:34 +0900

    Create Greeting.js

---
+++ src/components/Greeting.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+
+export default class About extends Component {
+    render() {
+        let resumeData = this.props.resumeData;
+        return (
+            <section id="greeting">
+                <div className="row">
+                    <div className="three columns">
+                        <img className="profile-pic" src="images/profilepic.jpg" alt=""/>
+                    </div>
+                    <div className="row">
+                        <div className="columns contact-details">
+                            <h2>校長挨拶</h2>
+                            <p className="address">
+                                校長のENDOです。こんばんはこんにちはおはようございます
+                                ごきげんよう。
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        );
+    }
+}
